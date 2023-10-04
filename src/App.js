156d08093974918314f76ef719bae8fad4467565
E-jiwoo <livei07@naver.com>
2023-10-04 19:06:44 +0900

    CHORE :: header correction - 헤더 수정

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-import Header from "./Components/Header/index";
-import Main from "./Components/Main/index";
-import Circle from "./Components/Circle/index";
-import {styled, createGlobalStyle} from "styled-components";
+import React from "react";
+import { createGlobalStyle } from "styled-components";
 import Introduce from "./Components/Introduce";
+import FirstPage from "./Components/Firstpage";
+import Header from "./Components/Header/index";
 import ReactFullpage from "@fullpage/react-fullpage";
-
-function App() {
-  return (
-    <>
-      <ReactFullpage
-        anchors={[
-          'main',
-          'introduce',
-        ]}
-        render={() => {
-          return (
-            <>
-              <GlobalStyle />
-              <div className="section">
-                <Container>
-                  <StyledHeader />
-                  <StyledCircle />
-                  <Main />
-                </Container>
-              </div>
-              <div className="section">
-                <Introduce/>
-              </div>
-            </>
-          )
-        }}
-      />
-    </>
-  );
-}
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -45,24 +15,28 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  width: 100vw;
-  height: 100vh;
-  background-color: #3e3d3f;
-`;
-
-const StyledHeader = styled(Header)`
-  position: relative;
-  z-index: 2;
-`;
-
-const StyledCircle = styled(Circle)`
-  position: relative;
-  z-index: 1;
-`;
+function App() {
+  return (
+    <>
+      <ReactFullpage
+        anchors={["main", "introduce"]}
+        render={() => {
+          return (
+            <>
+              <GlobalStyle />
+              <Header />
+              <div className="section">
+                <FirstPage />
+              </div>
+              <div className="section">
+                <Introduce />
+              </div>
+            </>
+          );
+        }}
+      />
+    </>
+  );
+}
 
 export default App;

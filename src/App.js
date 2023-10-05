b46279f3ce2f8f70ea5e4c91f5721fcccab4524c
최성훈 <chltjdgns1009@gmail.com>
2023-10-05 20:23:17 +0900

    ADD :: ui,common,assets - Components를 common과 ui로 나누었고 assets 폴더를 추가하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
-import Introduce from "./Components/Introduce";
-import FirstPage from "./Components/Firstpage";
-import Header from "./Components/Header/index";
+import FirstPage from "./Components/ui/Firstpage";
+import Header from "./Components/common/Header"
 import ReactFullpage from "@fullpage/react-fullpage";
+import Introduce from "./Components/ui/Introduce";
+import Projects from "./Components/ui/Projects";
 
 function App() {
   return (
     <>
       <Header />
       <ReactFullpage
-        anchors={["main", "introduce"]}
+        anchors={["main", "introduce","projects"]}
         render={() => {
           return (
             <>
               <GlobalStyle />
-
               <div className="section">
                 <FirstPage />
               </div>
               <div className="section">
                 <Introduce />
+              </div>
+              <div className="section">
+                <Projects />
               </div>
             </>
           );

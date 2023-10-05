720b564f008bcfe8dbe25c05afdfc179756c1876
최성훈 <chltjdgns1009@gmail.com>
2023-10-05 20:37:33 +0900

    ADD :: icon, contents - 마루 아이콘과 설명 글

--- src/Components/ui/Projects/index.js
+++ src/Components/ui/Projects/index.js
@@ -6,6 +6,11 @@ const Projects = () => {
   return (
     <Container>
       <Content>
+        <Text_content>
+          <Project>Project</Project>
+          <Maru>마루</Maru>
+          <Project_info>마루는 부산소마고의<br/>입학 지원서비스입니다.</Project_info>
+        </Text_content>
         <Icon_layout>
           <Icon src={maru_symbol} />
         </Icon_layout>
@@ -22,12 +27,13 @@ const Container = styled.div`
   align-items: center;
 `
 const Content = styled.div`
-  width: 70%;
+  display: flex;
+  gap: 400px;
 `
 const Icon_layout = styled.div`
   display: flex;
-  width: 368px;
-  height: 368px;
+  width: 150px;
+  height: 150px;
   padding: 90px 84px 91px 84px;
   justify-content: center;
   align-items: center;
@@ -39,5 +45,32 @@ const Icon = styled.img`
   width: 200px;
   height: 187px;
 `
-
+const Text_content = styled.div`
+  margin-top: 64px;
+`
+const Project = styled.p`
+  color: #6699ED;
+  font-family: Pretendard;
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`
+const Maru = styled.p`
+  color: var(--grey3, #3E3D3F);
+  font-family: Pretendard;
+  font-size: 65px;
+  font-style: normal;
+  margin-top: 8px;
+  font-weight: 700;
+  line-height: normal;
+`
+const Project_info = styled.p`
+  color: var(--grey3, #3E3D3F);
+  font-family: Pretendard;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`
 export default Projects;

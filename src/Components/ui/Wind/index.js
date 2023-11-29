5d5bd0a18574c23b03f01e6d5544ed7d6a16b634
E-jiwoo <livei07@naver.com>
2023-11-29 12:21:39 +0900

    CHORE :: Wind style 파일 - Wind index.js 안에 있는 styled-components를 style.js라는 파일로 따로 뺌

--- src/Components/ui/Wind/index.js
+++ src/Components/ui/Wind/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import styled from "styled-components";
-import bsm from "../../../assets/bsm.svg";
+import * as S from "./style";
 
 const Wind = () => {
   const [wind, setWind] = useState([]);
@@ -22,103 +21,28 @@ const Wind = () => {
   }, []);
 
   return (
-    <Container>
-      <Font>
-        <Title>우리의 바람이 당신에게 닿기를</Title>
-        <Div>
+    <S.Container>
+      <S.Font>
+        <S.Title>우리의 바람이 당신에게 닿기를</S.Title>
+        <S.Div>
           {wind.map((a) => (
-            <Box>
-              <Textbox>
-                <Square>
-                  <Number>{a.id}번째 바람</Number>
-                  <Text>{a.wind}</Text>
-                </Square>
-              </Textbox>
-            </Box>
+            <S.Box>
+              <S.Textbox>
+                <S.Square>
+                  <S.Number>{a.id}번째 바람</S.Number>
+                  <S.Text>{a.wind}</S.Text>
+                </S.Square>
+              </S.Textbox>
+            </S.Box>
           ))}
-        </Div>
-      </Font>
-    </Container>
+        </S.Div>
+      </S.Font>
+    </S.Container>
   );
 };
 
 Wind.defaultProps = {
   Wind: [],
 };
-
-const Div = styled.div`
-  width: 3000px;
-  height: 1800px;
-`;
-
-const Box = styled.div`
-  display: inline-block;
-  justify-content: flex-start;
-  padding: 10px;
-`;
-
-const Square = styled.div`
-  padding-top: 29px;
-  padding-bottom: 29px;
-  padding-left: 33px;
-  padding-right: 33px;
-`;
-
-const Text = styled.div`
-  position: relative;
-  font-size: 25px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  user-select: none;
-  text-align: left;
-  padding-top: 10px;
-`;
-
-const Number = styled.div`
-  position: relative;
-  color: var(--grey2, #929292);
-  font-family: Pretendard;
-  font-size: 21px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-  user-select: none;
-  text-align: left;
-`;
-
-const Textbox = styled.div`
-  position: relative;
-  top: 180px;
-  left: 100px;
-  width: auto;
-  height: auto;
-  border-radius: 24px;
-  background-color: rgba(255, 255, 255, 0.24);
-`;
-
-const Font = styled.div`
-  color: var(--white, #fff);
-  text-align: center;
-  font-family: Pretendard;
-`;
-const Title = styled.div`
-  font-size: 35px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  position: relative;
-  top: 110px;
-  user-select: none;
-`;
-
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-image: url(${bsm});
-  background-size: cover;
-  background-position: center;
-  overflow: hidden;
-`;
 
 export default Wind;
---
+++ src/Components/ui/Wind/style.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import bsm from "../../../assets/bsm.svg";
+
+export const Div = styled.div`
+  width: 3000px;
+  height: 1800px;
+`;
+
+export const Box = styled.div`
+  display: inline-block;
+  justify-content: flex-start;
+  padding: 10px;
+`;
+
+export const Square = styled.div`
+  padding-top: 29px;
+  padding-bottom: 29px;
+  padding-left: 33px;
+  padding-right: 33px;
+`;
+
+export const Text = styled.div`
+  position: relative;
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  user-select: none;
+  text-align: left;
+  padding-top: 10px;
+`;
+
+export const Number = styled.div`
+  position: relative;
+  color: var(--grey2, #929292);
+  font-family: Pretendard;
+  font-size: 21px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  user-select: none;
+  text-align: left;
+`;
+
+export const Textbox = styled.div`
+  position: relative;
+  top: 180px;
+  left: 100px;
+  width: auto;
+  height: auto;
+  border-radius: 24px;
+  background-color: rgba(255, 255, 255, 0.24);
+`;
+
+export const Font = styled.div`
+  color: var(--white, #fff);
+  text-align: center;
+  font-family: Pretendard;
+`;
+export const Title = styled.div`
+  font-size: 35px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  position: relative;
+  top: 110px;
+  user-select: none;
+`;
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: url(${bsm});
+  background-size: cover;
+  background-position: center;
+  overflow: hidden;
+`;

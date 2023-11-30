0ff4531ba31b409e04ec107de5f4ee8fd85629c8
E-jiwoo <livei07@naver.com>
2023-11-30 14:55:59 +0900

    CHORE :: Header style 파일
    - Header index.js 안에 있는 styled-components를 style.js라는 파일로 따로 뺌

--- src/Components/common/Header/index.js
+++ src/Components/common/Header/index.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import * as S from "./style";
 import React from "react";
 import black_logo from "../../../assets/black_logo.svg";
 import white_logo from "../../../assets/white_logo.svg";
@@ -7,9 +7,9 @@ function Header(props) {
   const Items = ["팀 소개", "팀 문화", "프로젝트", "팀 연혁", "멤버 소개"];
 
   return (
-    <HeaderContainer activeSection={props.activeSection}>
-      <Menu>
-        <Logo
+    <S.HeaderContainer activeSection={props.activeSection}>
+      <S.Menu>
+        <S.Logo
           src={
             props.activeSection === "introduce" ||
             props.activeSection === "culture" ||
@@ -22,7 +22,7 @@ function Header(props) {
           alt="logo"
         />
         {Items.map((item, index) => (
-          <MenuItem
+          <S.MenuItem
             key={index}
             className={
               props.activeSection === "introduce" ||
@@ -35,65 +35,11 @@ function Header(props) {
             }
           >
             {item}
-          </MenuItem>
+          </S.MenuItem>
         ))}
-      </Menu>
-    </HeaderContainer>
+      </S.Menu>
+    </S.HeaderContainer>
   );
 }
-
-const HeaderContainer = styled.div`
-  position: fixed;
-  top: 0;
-  z-index: 1000;
-  background-color: ${(props) =>
-    props.activeSection === "introduce" ||
-    props.activeSection === "culture" ||
-    props.activeSection === "projects" ||
-    props.activeSection === "history" ||
-    props.activeSection === "member"
-      ? "rgba(255, 255, 255, 0.06)"
-      : "rgba(62, 61, 63, 0.30)"};
-  border-bottom: 1px solid rgba(62, 61, 63, 0.3);
-  backdrop-filter: blur(5px);
-`;
-
-const Menu = styled.ul`
-  display: flex;
-  align-items: center;
-  color: black;
-  width: 100vw;
-  height: 60px;
-  font-size: 20px;
-  font-weight: 600;
-  margin-left: 170px;
-`;
-
-const MenuItem = styled.li`
-  list-style-type: none;
-  margin-right: 30px;
-  cursor: pointer;
-  position: relative;
-
-  color: ${(props) => (props.className === "active" ? "black" : "white")};
-
-  &:hover::before {
-    content: "";
-    position: absolute;
-    top: -8px;
-    left: -12px;
-    right: -12px;
-    bottom: -8px;
-    background: rgba(238, 238, 238, 0.6);
-    z-index: -1;
-    border-radius: 6px;
-  }
-`;
-
-const Logo = styled.img`
-  width: 170px;
-  height: 20px;
-  margin-right: 570px;
-`;
 
 export default Header;

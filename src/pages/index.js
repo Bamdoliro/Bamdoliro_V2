c80edfe9ad2f032ba85498c46de72082f6087123
최성훈 <chltjdgns1009@gmail.com>
2024-02-05 21:34:34 +0900

    CHOIR :: code 정리

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
-import Hash from "../components/Hash";
 import Header from "../components/Header"
 import Main from "./Layouts/Main"
+
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Mousewheel, Pagination } from 'swiper/modules';
+import { Mousewheel } from 'swiper/modules';
 import 'swiper/css'
+
 import Introduce from "./Layouts/Introduce";
 import Histories from "./Layouts/Histories";
 import Projects from "./Layouts/Projects";
@@ -21,15 +22,14 @@ const Home = () => {
     <ThemeProvider theme={{ activeSection }}>
       <Header  />
       <GlobalStyle />
-      {/* <Hash setActiveSection={setActiveSection} /> */}
       <Swiper
-        slidesPerView={1}
         direction="vertical"
         mousewheel={true}
         pagination={{
           clickable : true,
         }}
         modules={[Mousewheel]}
+        speed={1000}
       >
         <SwiperSlide>
           <Main />

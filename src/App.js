81d4f4b80275d2bf84d6b2416a52cb3de9a45622
최성훈 <chltjdgns1009@gmail.com>
2024-02-05 21:41:48 +0900

    CHOIR :: style.css to App.css - 전체 스타일을 관리하는 style.css의 이름을 App.css로 바꾸었습니다

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 import Home from "./pages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Introduce from "./pages/Layouts/Introduce/index";
-import Culture from "./pages/Layouts/Culture/index";
-import Projects from "./pages/Layouts/Projects/index";
-import Histories from "./pages/Layouts/Histories/index";
-import Member from "./pages/Layouts/Member/index";
-import "./style.css"
+import "./App.css"
 
 function App() {
   return (
@@ -14,11 +9,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/#introduce" element={<Introduce />} />
-          <Route path="/#projects" element={<Projects />} />
-          <Route path="/#culture" element={<Culture />} />
-          <Route path="/#member" element={<Member />} />
-          <Route path="/#histories" element={<Histories />} />
         </Routes>
       </BrowserRouter>
     </>
@@ -26,38 +16,3 @@ function App() {
 }
 
 export default App;
-/*import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
-import Home from "./pages/index";
-import Introduce from "./pages/Layouts/Introduce/index";
-import Culture from "./pages/Layouts/Culture/index";
-import Projects from "./pages/Layouts/Projects/index";
-import Histories from "./pages/Layouts/Histories/index";
-import Member from "./pages/Layouts/Member/index";
-
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <nav>
-          <Link to="/introduce">팀 소개</Link>
-          <Link to="/projects">프로젝트</Link>
-          <Link to="/culture">팀 문화</Link>
-          <Link to="/member">멤버 소개</Link>
-          <Link to="/histories">팀 연혁</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/introduce" element={<Introduce />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/culture" element={<Culture />} />
-          <Route path="/member" element={<Member />} />
-          <Route path="/histories" element={<Histories />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
-
-export default App;
-*/

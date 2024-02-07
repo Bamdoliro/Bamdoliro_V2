7f567e0e8b4afdd24a3352cf15179e6ca122a513
최성훈 <chltjdgns1009@gmail.com>
2024-02-07 19:17:28 +0900

    REPECTOR :: introduce to Culutre - introduce에서 culture페이지로 넘어가는걸 막았습니다

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React, {  useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
 import Header from "../components/Header"
 import Main from "./Layouts/Main"
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Mousewheel } from 'swiper/modules';
+import { Mousewheel, HashNavigation, Navigation } from 'swiper/modules';
 import 'swiper/css'
 
 import Introduce from "./Layouts/Introduce";
@@ -17,44 +17,53 @@ import Footer from "./Layouts/Footer"
 
 const Home = () => {
   const [activeSection, setActiveSection] = useState("");
+  const [textNumber, setTextNumber] = useState(0)
 
   return (
     <ThemeProvider theme={{ activeSection }}>
-      <Header  />
+      <Header />
       <GlobalStyle />
       <Swiper
+        hashNavigation={{
+          watchState: true,
+        }}
+        touchRatio={0}
         direction="vertical"
         mousewheel={true}
-        pagination={{
-          clickable : true,
-        }}
-        modules={[Mousewheel]}
+        navigation={true}
+        modules={[Mousewheel, HashNavigation, Navigation]}
         speed={1000}
+        slidesPerView={1}
       >
-        <SwiperSlide>
+        <SwiperSlide data-hash="slide1">
           <Main />
         </SwiperSlide>
-        <SwiperSlide>
-          <Introduce />
+        <SwiperSlide data-hash="slide2">
+          <Introduce textNumber={textNumber} setTextNumber={setTextNumber} />
         </SwiperSlide>
-        <SwiperSlide>
-          <Culture />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Projects />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Histories />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Member />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Wind />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Footer />
-        </SwiperSlide>
+        {
+          textNumber >= 5 ? <>
+            <SwiperSlide data-hash="slide3">
+              <Culture />
+            </SwiperSlide>
+            <SwiperSlide data-hash="slide4">
+              <Projects />
+            </SwiperSlide>
+            <SwiperSlide data-hash="slide5">
+              <Histories />
+            </SwiperSlide>
+            <SwiperSlide data-hash="slide6">
+              <Member />
+            </SwiperSlide>
+            <SwiperSlide data-hash="slide7">
+              <Wind />
+            </SwiperSlide>
+            <SwiperSlide data-hash="slide8">
+              <Footer />
+            </SwiperSlide></>
+            :
+            null
+          }
       </Swiper>
     </ThemeProvider>
   );
